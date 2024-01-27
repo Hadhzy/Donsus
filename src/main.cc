e3b36de18f7729bae3022945e7a78eb8086685e3
gaborhadhazy <gaborhadhazy07@gmail.com>
2024-01-27 17:44:18 +0000

    - Added basic Cmake system - Added basic lexer - Added test file

---
+++ src/main.cc
@@ -0,0 +1,18 @@
+// ENTRY POINT (main function)
+
+#include <iostream>
+#include "runner.h"
+
+#define VERSION_STRING "0.0.1"
+
+const std::string USAGE_TEXT = "TBD";
+
+void print_usage(){
+    std::cout << USAGE_TEXT << std::endl;
+}
+
+int main(int argc, char** argv) {
+    if (argc <= 1) print_usage();
+    std::cout << "DONSUS COMPILER: " << VERSION_STRING << std::endl;
+    return Du_Main(argc, argv);
+}
