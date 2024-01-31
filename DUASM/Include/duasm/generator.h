9801346af2eb2b5478bac62046f5e3761408416c
Tamas Hadhazy <helsgithub15@gmail.com>
2024-01-31 20:03:39 +0000

    - DUASM basic files. - Added SEMA.

---
+++ DUASM/Include/duasm/generator.h
@@ -0,0 +1,7 @@
+// Generate DUASM instructions from the data
+#ifndef DUASM_GENERATOR_H
+#define DUASM_GENERATOR_H
+
+void duasm_generate();
+
+#endif
---
+++ Include/sema.h
@@ -0,0 +1,8 @@
+#ifndef DONSUS_SEMA_H
+#define DONSUS_SEMA_H
+#include "parser.h"
+
+
+donsus_ast& donsus_sema(donsus_ast& ast);
+
+#endif // DONSUS_SEMA_H
