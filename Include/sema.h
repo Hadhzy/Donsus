9b293d7f5431b4e6c28e654e163977587fbff13e
Tamas Hadhazy <helsgithub15@gmail.com>
2024-02-18 14:57:13 +0000

    Got rid of codegen and symbol-table.

--- Include/sema.h
+++ Include/sema.h
@@ -1,6 +1,5 @@
 #ifndef DONSUS_SEMA_H
 #define DONSUS_SEMA_H
-#include "../Include/symbol_table.h"
 #include "../src/utility/handle.h"
 #include "../src/utility/memory_alloc.h"
 #include "parser.h"
@@ -8,6 +7,8 @@
 DonsusParser::end_result donsus_sema(DonsusParser::end_result ast,
                                        std::string &file_name);
 
+/*
 utility::handle<donsus_symtable> donsus_sym(std::string &file_name);
+*/
 
 #endif // DONSUS_SEMA_H
