748af5c6a088ca4b667dc3fb9253195e32c7e769
gaborhadhazy <gaborhadhazy07@gmail.com>
2024-03-28 16:06:37 +0000

    evaulate number expressions basic algorithm

--- src/ast/node.h
+++ src/ast/node.h
@@ -126,9 +126,7 @@ struct expression {
   donsus_token value;
 };
 
-struct print_expr {
-
-};
+struct print_expr {};
 
 using node_properties =
     utility::property<variable_decl, number_expr, function_decl, function_def>;
@@ -139,6 +137,35 @@ struct node : node_properties {
       children;          // size type in the future
   donsus_node_type type; // This is the node's type
   DONSUS_TYPE real_type; // This type is assigned during type checking
+
+  int constant_propagation(const utility::handle<node> &node) {
+    if (node->children.size() == 0 &&
+        node->type.type ==
+            donsus_ast::donsus_node_type::DONSUS_NUMBER_EXPRESSION) {
+      // If it's a number expression, return its value
+      return std::stoi(node->get<number_expr>().value.value);
+    } else {
+      auto leftValue = constant_propagation(node->children[0]);
+      auto rightValue = constant_propagation(node->children[1]);
+
+      // Perform the operation based on the operator
+      auto op = node->get<number_expr>().value.value;
+      if (op == "+") {
+        return leftValue + rightValue;
+      } else if (op == "-") {
+        return leftValue - rightValue;
+      } else if (op == "*") {
+        return leftValue * rightValue;
+      } else if (op == "/") {
+        if (rightValue == 0) {
+          throw std::runtime_error("Division by zero");
+        }
+        return leftValue / rightValue;
+      } else {
+        throw std::runtime_error("Invalid operator");
+      }
+    }
+  }
 };
-} // namespace donsus_ast
+}; // namespace donsus_ast
 #endif
