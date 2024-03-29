ac9d29c6239d743da746293cfcd02efd901e1c05
gaborhadhazy <gaborhadhazy07@gmail.com>
2024-03-29 17:44:00 +0000

    add more teests

--- donsus_test/typecheck/test_return.cc
+++ donsus_test/typecheck/test_return.cc
@@ -1,6 +1,6 @@
+#include "../../Include/codegen/codegen.h"
 #include "../Include/sema.h"
 #include <gtest/gtest.h>
-#include "../../Include/codegen/codegen.h"
 /*
 
  * */
@@ -18,10 +18,42 @@ TEST(ReturnStatementTypeCheckScalarCorrect, ReturnStatementTypecheck) {
       { parse_result->traverse(donsus_sym, assign_type_to_node, sym_global); });
 }
 
+TEST(ReturnStatementTypeCheckIdentifierCorrect, ReturnStatementTypecheck) {
+  std::string a = R"(
+    b:int = 12;
+    def a() ->int {
+    return b;
+}
+)";
+  DonsusParser::end_result parse_result = Du_Parse(a);
+  utility::handle<DonsusSymTable> sym_global = new DonsusSymTable();
+
+  parse_result->init_traverse();
+  EXPECT_NO_THROW(
+      { parse_result->traverse(donsus_sym, assign_type_to_node, sym_global); });
+}
+
 TEST(ReturnStatementTypeCheckScalarIncorrect, ReturnStatementTypecheck) {
   std::string a = R"(
     def a() ->char{
     return 1;
+}
+)";
+
+  DonsusParser::end_result parse_result = Du_Parse(a);
+  utility::handle<DonsusSymTable> sym_global = new DonsusSymTable();
+
+  parse_result->init_traverse();
+  EXPECT_THROW(
+      { parse_result->traverse(donsus_sym, assign_type_to_node, sym_global); },
+      ReturnTypeException);
+}
+
+TEST(ReturnStatementTypeCheckIdentifierIncorrect, ReturnStatementTypecheck) {
+  std::string a = R"(
+    b:int = 12;
+    def a() ->char{
+    return b;
 }
 )";
 
