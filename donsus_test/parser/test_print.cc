366050e8abd1aed19d44cb6354cfd08bfa803016
Tamas Hadhazy <helsgithub15@gmail.com>
2024-04-12 18:00:57 +0100

    Rename printf in tests

--- donsus_test/parser/test_print.cc
+++ donsus_test/parser/test_print.cc
@@ -7,8 +7,9 @@
  * */
 TEST(TestPrintLiteral, TestPrintSystem) {
   std::string a = R"(
-        print("a");
+        printf("a");
     )";
   DonsusParser::end_result result = Du_Parse(a);
-  EXPECT_EQ(donsus_ast::donsus_node_type::DONSUS_STRING_EXPRESSION, result->get_nodes()[0]->children[0]->type.type);
+  EXPECT_EQ(donsus_ast::donsus_node_type::DONSUS_STRING_EXPRESSION,
+            result->get_nodes()[0]->children[0]->type.type);
 }
